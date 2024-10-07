95213f5d1eddb3eebe40ce4b29993d7b2abb404a
HafflePaffle <Hafsteinn.pe@gmail.com>
2024-10-07 09:53:39 +0200

    Big spins

--- Assets/Scripts/SpinActivator.cs
+++ Assets/Scripts/SpinActivator.cs
@@ -7,7 +7,7 @@ public class SpinActivator : MonoBehaviour
     public KeyCode key;
     private float time = 0.1f;
     public float degreesToTurn;
-    private Quaternion startRot;
+    private Transform startRot;
     float r;
     bool started = false;
     private float speed = 6;//Takes 360 seconds for a full rotation at speed == 1. 60 seconds is speed == 6
@@ -18,7 +18,7 @@ public class SpinActivator : MonoBehaviour
 
     private void Awake()
     {
-        startRot = transform.rotation;
+        startRot = this.transform;
     }
 
     void Update()
@@ -39,7 +39,8 @@ public class SpinActivator : MonoBehaviour
 
     private void StartSpin()
     {
-        float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, startRot.eulerAngles.y + movement, ref r, time);
-        this.transform.rotation = Quaternion.Euler(parentTransform.eulerAngles.x, angle, parentTransform.eulerAngles.z);
+        //float angle = Mathf.SmoothDampAngle(transform.localEulerAngles.y, startRot.localEulerAngles.y + movement, ref r, time);
+        //this.transform.rotation = Quaternion.Euler(parentTransform.eulerAngles.x, angle, parentTransform.eulerAngles.z);
+        this.transform.localRotation = Quaternion.AngleAxis(movement, Vector3.up);
     }
 }
