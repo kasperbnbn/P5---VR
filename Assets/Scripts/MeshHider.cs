dbe91beea976af97ebd64395b3e5ca0b1fdc57a0
HafflePaffle <Hafsteinn.pe@gmail.com>
2024-10-28 14:31:40 +0100

    a

---
+++ Assets/Scripts/MeshHider.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeshHider : MonoBehaviour
+{
+    [SerializeField] private GameObject[] meshToHide;
+    private bool cooldown;
+
+    public void Hide()
+    {
+        if (!cooldown)
+        {
+            cooldown = true;
+            foreach (GameObject m in meshToHide)
+            {
+                MeshRenderer meshRenderer = m.GetComponent<MeshRenderer>();
+                meshRenderer.enabled = !meshRenderer.enabled;
+            }
+            StartCoroutine(CoolDown());
+        }
+    }
+
+    IEnumerator CoolDown()
+    {
+        yield return new WaitForSeconds(1f);
+        cooldown = false;
+    }
+}
