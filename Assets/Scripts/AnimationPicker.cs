eec085fee588fc9f64d86bdfe61f335ce545c2eb
HafflePaffle <Hafsteinn.pe@gmail.com>
2024-10-30 13:21:05 +0100

    a

--- Assets/Scripts/AnimationPicker.cs
+++ Assets/Scripts/AnimationPicker.cs
@@ -6,11 +6,19 @@ public class AnimationPicker : MonoBehaviour
 {
     private Animator animator;
     private int variantNumber;
+    
     void OnEnable()
     {
         animator = GetComponent<Animator>();
-        variantNumber = Random.Range(0, 10);
+        variantNumber = Random.Range(0, 3);
+        float offset = Random.Range(0, 1);
 
+        StartCoroutine(Starting(offset));
+    }
+
+    IEnumerator Starting(float time)
+    {
+        yield return new WaitForSeconds(time);
         animator.SetInteger("Variant", variantNumber);
     }
 }
